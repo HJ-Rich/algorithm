fa4eed825a960f6eea5c74cacf06d59405669ef3
Richard JEON <ztzy1907@gmail.com>
2021-04-14 09:00:19 +0900

    2021.04.14 09:00 오픈채팅방 테스트케이스 32개중 6개 시간초과

--- src/etc/Open_Chatting_Room.java
+++ src/etc/Open_Chatting_Room.java
@@ -1,26 +1,54 @@
 package etc;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 // https://programmers.co.kr/learn/courses/30/lessons/42888
 public class Open_Chatting_Room {
 
     public static void main(String[] args) {
-        String[] w = new String[] {"Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"};
+        String[] record = new String[] {"Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"};
 
-        for (int i = 0; i < w.length; i++) {
-            if(w[i].split(" ")[0].equals("Change")) {
-                for(int j = 0; j < w.length; j++) {
-                    String uid = w[i].split(" ")[1];
-                    if(w[j].split(" ")[1].equals(uid)) {
-                        String temp = w[j].split(" ")[0] + " " + w[j].split(" ")[1] + " " + w[i].split(" ")[2];
-                        w[j] = temp;
+        Map<String, String> idmap = new HashMap<>();
+        for (int i = 0; i < record.length; i++) {
+            String[] temp = record[i].split(" ");
+            switch (temp[0]) {
+                case "Enter":
+                case "Change":
+                    idmap.put(temp[1], temp[2]);
+                    break;
+            }
+        }
+
+
+        for (int i = 0; i < record.length; i++) {
+            if(record[i].split(" ")[0].equals("Change")) {
+                for(int j = 0; j < record.length; j++) {
+                    String uid = record[i].split(" ")[1];
+                    if(record[j].split(" ")[1].equals(uid)) {
+                        String temp = record[j].split(" ")[0] + " " + record[j].split(" ")[1] + " " + record[i].split(" ")[2];
+                        record[j] = temp;
                     }
                 }
             }
         }
 
-        StringBuilder stringBuilder = new StringBuilder();
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < record.length; i++) {
+            String[] temp = record[i].split(" ");
+            switch (temp[0]) {
+                case "Enter":
+                    answer.append(idmap.get(temp[1]) + "님이 들어왔습니다.\n");
+                    break;
+                case "Leave":
+                    answer.append(idmap.get(temp[1]) + "님이 나갔습니다.\n");
+                    break;
+            }
+        }
 
-
+        String[] answer2 = answer.toString().split("\n");
+        System.out.println("gg");
 
     }
 
