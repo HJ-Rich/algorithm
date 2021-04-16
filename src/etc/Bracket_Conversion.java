250efb3176f133746846cfc14be0e2771c69ee65
Richard JEON <ztzy1907@gmail.com>
2021-04-16 13:00:06 +0900

    2021.04.16 12:59 괄호 변환 1차 테이스케이스 통과

--- src/etc/Bracket_Conversion.java
+++ src/etc/Bracket_Conversion.java
@@ -21,12 +21,7 @@ public class Bracket_Conversion {
         2. 문자열 w를 두 "균형잡힌 괄호 문자열" u, v로 분리합니다. 단, u는 "균형잡힌 괄호 문자열"로 더 이상 분리할 수 없어야 하며, v는 빈 문자열이 될 수 있습니다.
         3. 문자열 u가 "올바른 괄호 문자열" 이라면 문자열 v에 대해 1단계부터 다시 수행합니다.
           3-1. 수행한 결과 문자열을 u에 이어 붙인 후 반환합니다.
-        4. 문자열 u가 "올바른 괄호 문자열"이 아니라면 아래 과정을 수행합니다.
-          4-1. 빈 문자열에 첫 번째 문자로 '('를 붙입니다.
-          4-2. 문자열 v에 대해 1단계부터 재귀적으로 수행한 결과 문자열을 이어 붙입니다.
-          4-3. ')'를 다시 붙입니다.
-          4-4. u의 첫 번째와 마지막 문자를 제거하고, 나머지 문자열의 괄호 방향을 뒤집어서 뒤에 붙입니다.
-          4-5. 생성된 문자열을 반환합니다.
+
          */
         //----------------------------------------------------------------
 
@@ -45,7 +40,7 @@ public class Bracket_Conversion {
 
         // u가 완성되었는지 여부를 판별할 boolean 객체
         boolean isUComplete = false;
-        boolean isRightV = true;
+        boolean isRightU = true;
 
         // 최소 단위로 u를 분리해내기 위한 int 객체 둘 선언.
         int left = 0;
@@ -62,7 +57,6 @@ public class Bracket_Conversion {
                 if (c == '(') vLeft++;
                 else if (c == ')') vRight++;
                 v.append(c);
-                if(right > left) isRightV = false;
             }
 
             // u가 완성되지 않았을 경우 u에 char를 쌓는다.
@@ -70,22 +64,35 @@ public class Bracket_Conversion {
                 if (c == '(') left++;
                 else if (c == ')') right++;
                 u.append(c);
+                if(right > left) isRightU = false;
                 if(left == right) isUComplete = true;
             }
         }
 
         // 올바른 괄호 문자열일 경우 그냥 붙인다
-        if(isRightV) {
+        if(isRightU) {
             answerBuilder.append(u);
         }
 
+
+        /*
+        4. 문자열 u가 "올바른 괄호 문자열"이 아니라면 아래 과정을 수행합니다.
+          4-1. 빈 문자열에 첫 번째 문자로 '('를 붙입니다.
+          4-2. 문자열 v에 대해 1단계부터 재귀적으로 수행한 결과 문자열을 이어 붙입니다.
+          4-3. ')'를 다시 붙입니다.
+          4-4. u의 첫 번째와 마지막 문자를 제거하고, 나머지 문자열의 괄호 방향을 뒤집어서 뒤에 붙입니다.
+          4-5. 생성된 문자열을 반환합니다.
+         */
         // 올바른 괄호 문자열이 아닐 경우 처리
-        else if(!isRightV) {
-
+        else if(!isRightU) {
+            StringBuilder uBuilder = new StringBuilder("(");
+            for (int i = 0; i < u.length(); i++) {
+                if(u.charAt(i) == '(') uBuilder.append(")");
+                else uBuilder.append("(");
+            }
+            uBuilder.append(")");
+            answerBuilder.append(uBuilder);
         }
-
-
-
 
         // 재귀적으로 수행한다.
         solution(v.toString(), answerBuilder);
