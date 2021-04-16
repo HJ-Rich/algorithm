41aceffeb6ea3d3940f9487869749ce4ca6c8d08
Richard JEON <ztzy1907@gmail.com>
2021-04-16 20:55:26 +0900

    2021.04.16 20:55 주석 제거.

--- src/etc/Bracket_Conversion.java
+++ src/etc/Bracket_Conversion.java
@@ -4,32 +4,11 @@ package etc;
 public class Bracket_Conversion {
     public static void main(String[] args) {
         StringBuilder answerBuilder = new StringBuilder();
-        new Bracket_Conversion().solution("()))((()", answerBuilder);
+        new Bracket_Conversion().solution2("()))((()", answerBuilder);
         System.out.println(answerBuilder);
     }
 
-    private void solution(String p, StringBuilder answerBuilder) {
-        // 균형잡힌 괄호 문자열이 주어진다.
-        // 즉, 문자열은 괄호로만 구성되며, 열기와 닫기의 갯수가 맞다는 뜻이다.
-        // 이를 열고 닫는 짝까지 맞춰내어 올바른 괄호 문자열로 만들어 반환하는 것이 문제다.
-
-        // 균형잡힌 괄호 문자열 -> 올바른 괄호 문자열 변환 알고리즘은 문제에 제시되었다.
-        // 이를 코드로 작성해 내는 것이 문제다.
-
-        /*
-        1. 입력이 빈 문자열인 경우, 빈 문자열을 반환합니다.
-        2. 문자열 w를 두 "균형잡힌 괄호 문자열" u, v로 분리합니다. 단, u는 "균형잡힌 괄호 문자열"로 더 이상 분리할 수 없어야 하며, v는 빈 문자열이 될 수 있습니다.
-        3. 문자열 u가 "올바른 괄호 문자열" 이라면 문자열 v에 대해 1단계부터 다시 수행합니다.
-          3-1. 수행한 결과 문자열을 u에 이어 붙인 후 반환합니다.
-
-         */
-        //----------------------------------------------------------------
-
-        // 2번 조건에 따라 주어진 문자열을 균형잡힌 괄호 문자열 2개로 분리해야 한다.
-        // 먼저 왼쪽을 최소단위로 쪼개고 쪼갠 결과가 올바른이면 붙이고 남은쪽을 다시 쪼개는식.
-        // 쪼갠 결과가 올바르지 않다면 4번의 과정을 수행.
-
-        // 재귀 브레이커
+    private void solution2(String p, StringBuilder answerBuilder) {
         if(p.equals("")) {
             return;
         }
@@ -95,7 +74,7 @@ public class Bracket_Conversion {
         }
 
         // 재귀적으로 수행한다.
-        solution(v.toString(), answerBuilder);
+        solution2(v.toString(), answerBuilder);
     }
 
 
