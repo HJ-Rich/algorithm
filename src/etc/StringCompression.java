c026da4becc71d60a24b755bab9ef9edacfaf537
Richard JEON <ztzy1907@gmail.com>
2021-04-29 07:52:02 +0900

    2021.04.29 07:51 문자열 압축 통과

--- src/etc/StringCompression.java
+++ src/etc/StringCompression.java
@@ -1,87 +1,72 @@
 package etc;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 // https://programmers.co.kr/learn/courses/30/lessons/60057
 public class StringCompression {
 
     public static void main(String[] args) {
-        String s = "xababcdcdababcdcd";
-        int answer = mySolution(s);
-        System.out.println("정답은~~~" + answer);
+        System.out.println("정답은~~~ : " + solution("xababcdcdababcdcd"));
     }
 
-    private static int mySolution(String s) {
-        int result = s.length();
+    public static int solution(String s) {
+        int answer = s.length();                                    // 맨 처음엔 압축하지 않은 최대 길이를 담는다.
 
-        // 나누어 떨어지는 길이로만 압축이 가능하다.
+        for (int i = 1; i <= s.length() / 2; i++) {                 // 1부터 문자열 길이의 절반까지의 단위로 잘라 압축한 결과 중 가장 짧은 결과를 구한다.
+            System.out.println("i = " + i);
+            String compressed = compressByLength(s, i);             // i를 기준으로 압축한 결과를 반환하는 메소드를 구성한다.
+            int compressedLength = compressed.length();             // 압축 결과 문자열의 길이다.
 
-        // 나누어 떨어지는 몫 구하기
-        List<Integer> shareList = getShareList(s);
+            System.out.printf("압축 대상 : %s, 압축 단위 : %s, 압축 결과 : %s, : %s", s, i+"", compressed, compressedLength+"\n");
 
-        for (int i = 0; i < shareList.size(); i++) {
+            if(compressedLength < answer) answer = compressedLength; // 압축 결과가 더 짧다면 answer에 대입한다.
+        }
 
-            int length = shareList.get(i);
+        return answer;                                              // 최종 결과를 반환한다.
+    }
 
-            // 구해온 몫을 길이로 잘라서 리스트에 담는다
-            List<String> compressList = new ArrayList<>();
-            for (int j = 0; j < s.length() / length; j++) {
-                compressList.add(s.substring(j * length, j * length + length));
+    private static String compressByLength(String target, int length) {
+        StringBuffer result = new StringBuffer();
+
+        int counter = 1;
+        boolean isFinal = false;
+
+        for (int i = 0; i < target.length(); i++) {
+            int index = i * length;
+            String now = "";
+            String next = "";
+
+            try {
+                now = target.substring(index, index + length);
+            }
+            catch (IndexOutOfBoundsException e) {
+                now = target.substring(index);
+                isFinal = true;
             }
 
-            // 리스트에서 하나씩 꺼내어 압축 결과를 만들어낸다.
-            StringBuilder compressResult = new StringBuilder();
-            for (int j = 0; j < compressList.size(); j++) {
-
-                int numberOfElement = 1;
-                int index = j;
+            if(!isFinal) {
                 try {
-                    while (true) {
-                        String a = compressList.get(index);
-                        String b = compressList.get(index + 1);
-                        if (a.equals(b)) {
-                            index++;
-                            numberOfElement++;
-                        }
-                        else {
-                            break;
-                        }
-                    }
-                }
-                // 마지막 요소가 비교할 대상을 찾지 못하며 IOBE 발생
-                // 그와 동시에 마지막 요소가 추가되지 못하여 마지막이 줄어든거네
-                catch(Exception e) {
-                }
-
-                if(numberOfElement > 1) {
-                    j = j + numberOfElement - 1;
-                    compressResult.append(numberOfElement + compressList.get(j));
-                }
-                else {
-                    compressResult.append(compressList.get(j));
+                    next = target.substring(index + length, index + length + length);
+                } catch (IndexOutOfBoundsException e) {
+                    next = target.substring(index + length);
+                    isFinal = true;
                 }
             }
-            HashSet
-            // 압축된 결과의 길이가 result 보다 작으면 result를 교체한다.
-            if(result > compressResult.length()) {
-                result = compressResult.length();
+
+            if(now.equals(next)) {
+                counter++;
+                continue;
             }
-
-            System.out.println(length + "로 나눴을 때! : " + compressResult.length() + " : " + compressResult);
+            else {
+                String stringToAppend = (counter > 1) ? counter + now : now;
+                result.append(stringToAppend);
+                if(isFinal) {
+                    result.append(next);
+                    break;
+                }
+                counter = 1;
+            }
         }
-        return result;
-    }
 
-    private static List<Integer> getShareList(String s) {
-        List<Integer> result = new ArrayList<>();
-        int target = s.length();
-        for (int i = 1; i < target; i++) {
-            if(target % i == 0) result.add(i);
-        }
-        return result;
+        return result.toString();
     }
 
 }
