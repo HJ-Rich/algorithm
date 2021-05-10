f87bfbb0b6ad0ce69b2729a715f63e5cc0c364c9
Richard JEON <ztzy1907@gmail.com>
2021-05-10 13:04:44 +0900

    2021.05.10 13:04 인형뽑기 진행중

--- src/etc/ClawMachine.java
+++ src/etc/ClawMachine.java
@@ -1,16 +1,19 @@
 package etc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 // https://programmers.co.kr/learn/courses/30/lessons/64061
 public class ClawMachine {
 
     public static void main(String[] args) {
-        System.out.println(solution(new int[][]{{0, 0, 0, 0, 0},{0, 0, 1, 0, 3},{0, 2, 5, 0, 1},{4, 2, 4, 4, 2},{3, 5, 1, 3, 1}}, new int[]{1, 5, 3, 5, 1, 2, 1, 4}));
+        System.out.println(solution(new int[][]{{0, 0, 0, 0, 0}, {0, 0, 1, 0, 3}, {0, 2, 5, 0, 1}, {4, 2, 4, 4, 2}, {3, 5, 1, 3, 1}}, new int[]{1, 5, 3, 5, 1, 2, 1, 4}));
     }
 
     private static int solution(int[][] board, int[] moves) {
         int result = 0;
 
-        int stackNumber = 0;
+        List<Integer> stackNumberList = new ArrayList<>();
 
         for (int i = 0; i < moves.length; i++) {
             int move = moves[i];
@@ -18,14 +21,25 @@ public class ClawMachine {
             for (int j = 0; j < board.length; j++) {
                 int target = board[j][move-1];
                 if(target != 0) {
-                    if(stackNumber == target) {
-                        result += 2;
-                        stackNumber = 0;
-                    }
-                    else {
-                        stackNumber = target;
-                    }
+                    stackNumberList.add(target);
                     board[j][move-1] = 0;
+                }
+            }
+
+        }
+
+        boolean isExploded = true;
+        while(isExploded) {
+            for (int i = 0; i < stackNumberList.size(); i++) {
+                if(i < stackNumberList.size() - 1) {
+                    int now = stackNumberList.get(i);
+                    int next = stackNumberList.get(i + 1);
+                    if(now == next) {
+                        result += 2;
+                        stackNumberList.remove(i);
+                        stackNumberList.remove(i + 1);
+                        i += -2;
+                    }
                 }
             }
         }
