2031d66da9284138a0f1bf2c391e7290e0db0e81
Richard JEON <ztzy1907@gmail.com>
2021-05-31 01:49:22 +0900

    2021.05.31 01:49 신규 아이디 추천 모범 답안 복기

--- src/programmers/level1/신규아이디추천/NewIdRecommend.java
+++ src/programmers/level1/신규아이디추천/NewIdRecommend.java
@@ -103,23 +103,47 @@ public class NewIdRecommend {
 }
 
 /*
-배워야할 타인의 답 1
+배워야할 타인의 답 1 - 김나현, 김윤옥님
+// 정규식을 제대로 사용하는 모범 사례다.
+// 복기하는 것만으로 큰 공부가 됐다.
 class Solution {
     public String solution(String new_id) {
         String answer = "";
+
+        // Step 1. 전체 소문자화
         String temp = new_id.toLowerCase();
 
+        // Step 2. 영소문자, 숫자, -_. 이외 문자 제거
+        // replaceAll 은 문자열 전체에 대해 탐색 및 변경을 수행.
+        // [] 안에 정규식을 넣음.
+        // a-z는 a부터 z까지 모든 영소문자, 0-9는 숫자를 의미, -_.는 각각의 특수문자 자체를 의미.
+        // ^는 Not을 의미하므로 ^ 이후의 정규표현식에 해당하지 않을 경우 두번째 파라미터인 ""로 대체됨.
         temp = temp.replaceAll("[^-_.a-z0-9]","");
-        System.out.println(temp);
+
+        // Step 3. .. 을 .로 교체
+        // [.] 점 하나가 {2,} 2번 이상 반복이라는 의미. 따라서 ..이던 ....이던 두번째 파라미터인 .로 교체됨
         temp = temp.replaceAll("[.]{2,}",".");
+
+        // Step 4. 맨 앞 또는 맨 뒤의 . 제거
+        // []안에 ^가 있으면 Not 의 의미
+        // []앞에 ^가 있으면 시작의 의미.
+        // 따라서 [^.] 이면 .이 아니면 match이고, ^[.] 이면 .로 시작하면 match이다.
+        // []뒤에 $가 오면 끝을 의미. [.]$는 .으로 끝나면 match.
+        // | 는 또는을 의미. 종합하면 (^[.] = .으로 시작한다) (| = 또는) ([.]$ = .으로 끝난다)
         temp = temp.replaceAll("^[.]|[.]$","");
-        System.out.println(temp.length());
+
+        // Step 5. 빈 문자열이면 "a"를 할당.
         if(temp.equals(""))
             temp+="a";
+
+        // Step 6. 길이가 15를 초과하면 잘라내고 맨 앞 뒤 .을 다시 제거.
+        // 이 때는 맨 앞 글자의 변화는 일어나지 않기 때문에 substring이후엔 replace("[.]$", "")만 해도 될 듯 하다.
         if(temp.length() >=16){
             temp = temp.substring(0,15);
             temp=temp.replaceAll("^[.]|[.]$","");
         }
+
+        // Step 7. 길이가 2 이하면 3이 될 때까지 마지막 문자를 더해준다.
         if(temp.length()<=2)
             while(temp.length()<3)
                 temp+=temp.charAt(temp.length()-1);
@@ -132,7 +156,12 @@ class Solution {
 
 
 /*
-배워야할 타인의 답 2
+배워야할 타인의 답 2 - "-"님
+실무 경험이 늘어날수록, 알고리즘을 풀면서도 유지보수, 확장성 및 객체지향적 설계를 고려하게 되는 경험을 한다.
+단순히 문제를 풀어내는 것을 넘어서서 스스로 만족할만한, 업무에도 적용 가능할법한 코드를 짜고 싶어진다.
+그리고 이 코드는 정규표현식의 사용도 그렇지만 메소드 체이닝과 유지 보수 확장성, 가독성 측면에서
+정말 실무에서 사용해도 충분하다고 느껴질 정도의 아름다운 코드라고 느껴진다.
+실제 필터를 적용해야할 여러 방면에서 이런식으로 클래스를 나눠서 생성시점에 메소드 체이닝을 활용할 수 있는 가능성을 배웠다.
 
 class Solution {
     public String solution(String new_id) {
