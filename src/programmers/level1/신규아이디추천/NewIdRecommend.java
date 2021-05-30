93126068c145791c90e340814706fecc464a324d
Richard JEON <ztzy1907@gmail.com>
2021-05-31 01:57:56 +0900

    Update NewIdRecommend.java

--- src/programmers/level1/신규아이디추천/NewIdRecommend.java
+++ src/programmers/level1/신규아이디추천/NewIdRecommend.java
@@ -1,6 +1,7 @@
 package programmers.level1.신규아이디추천;
 
 // 신규 아이디 추천
+// https://programmers.co.kr/learn/courses/30/lessons/72410
 public class NewIdRecommend {
 
     public static void main(String[] args) {
