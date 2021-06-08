38b6316d5a4404f7320802f23602f20a71e5a500
Richard JEON <ztzy1907@gmail.com>
2021-06-08 22:20:03 +0900

    2021.06.08 22:19 내적 통과

---
+++ src/programmers/진행중/내적/InnerProduct.java
@@ -0,0 +1,22 @@
+package programmers.진행중.내적;
+
+// 내적
+// https://programmers.co.kr/learn/courses/30/lessons/70128
+public class InnerProduct {
+
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{1, 2, 3, 4}, new int[]{-3, -1, 0, 2}) == 3);
+        System.out.println(solution(new int[]{-1, 0, 1}, new int[]{1, 0, -1}) == -2);
+    }
+
+    public static int solution(int[] a, int[] b) {
+        int answer = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            answer += a[i] * b[i];
+        }
+
+        return answer;
+    }
+
+}
